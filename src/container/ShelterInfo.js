8d229fa944103ae2f0aab903f4983ae203cfbd7c
dphuong98 <32475252+dphuong98@users.noreply.github.com>
2020-11-09 17:50:13 +0700

    Routing correction
    -Added exact to <Route> to prevent unexpected routing

---
+++ src/container/ShelterInfo.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import {Button } from 'antd';
+
+import '../App.css';
+
+
+class ShelterInfo extends Component {
+    render (){
+        return (
+          <h1>temp</h1>
+        );
+      }
+}
+
+export default ShelterInfo;
