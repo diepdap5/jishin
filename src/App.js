b57ac451bd2195f6e60ad11ead7fd9725304fe5d
dphuong98 <32475252+dphuong98@users.noreply.github.com>
2020-11-09 17:22:38 +0700

    Created routing
    +Create routing
    +Organize files to boost work efficency

--- src/App.js
+++ src/App.js
@@ -1,42 +1,22 @@
 // import logo from './logo.svg';
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import {Button } from 'antd';
+
 import './App.css';
-import SimpleMap from './Map'
+import MapTemp from './container/MapTemp'
+
+
 class App extends Component {
-  state = {
-    my_latitude: 21.020671999999998,
-    my_longitude: 105.84391679999999,
-    zoom : 15
-  }
-  handleUserLocation = () => {
-    var my_latitude;
-    var my_longitude;
-  
-    navigator.geolocation.getCurrentPosition(
-      position => {
-        my_latitude = position.coords.latitude;
-        this.setState({
-          my_latitude: my_latitude
-        })
-        console.log("My latitude: "+ this.state.my_latitude);
-        my_longitude = position.coords.longitude;
-        this.setState({
-          my_longitude: my_longitude
-        })
-        console.log("My longitude: "+ this.state.my_longitude);
-      }
-    );
-  };
-  render (){
+  render() {
     return (
-      <div className="App">
-        {/* <Button type="primary" onClick = {this.handleUserLocation}>ClickMe</Button> */}
-        <SimpleMap center={{lat:this.state.my_latitude, lng:this.state.my_longitude}}/>
-      </div>
+      <BrowserRouter>
+        <Fragment>
+          <Route path="/" component={MapTemp}/>
+        </Fragment>
+      </BrowserRouter>
     );
   }
-  
 }
 
 export default App;
