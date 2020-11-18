c51f0367fce82704193edfcd7587af9845159717
khanhlyvu30 <khanhlyvu30@gmail.com>
2020-11-19 01:07:08 +0700

    Change page to pink theme and format occured date

--- src/container/LogPage.js
+++ src/container/LogPage.js
@@ -1,7 +1,7 @@
 import "antd/dist/antd.css";
 import "../App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import { Layout, Menu, Table } from "antd";
+import { Layout, Menu, Table, Tag } from "antd";
 import { HomeOutlined, NotificationOutlined } from "@ant-design/icons";
 import ShelterInfo from "../container/ShelterInfo";
 import { Component } from "react";
@@ -47,20 +47,27 @@ class LogPage extends Component {
   }
 
   componentDidMount() {
-    axios
-      .get(`https://5fa8a7c7c9b4e90016e697f4.mockapi.io/api/jishin/log`)
-      .then((res) => {
-        const posts = res.data.map((obj) => ({
+    axios.get('https://5fa8a7c7c9b4e90016e697f4.mockapi.io/api/jishin/log').then((res) => {
+      const posts = res.data.map((obj) => {
+        let timeLeft = '';
+        const difference = obj.occure_time - Date.now();
+        if (difference > 0) {
+          if (difference > 24 * 60 * 60) timeLeft = `${Math.floor(difference / 24 / 60 / 60)} days left`;
+          else if (difference > 60 * 60) timeLeft = `${Math.floor(difference / 60 / 60)} hours left`;
+          else if (difference > 60) timeLeft = `${Math.floor(difference / 60)} minutes left`;
+          else timeLeft = `${difference} seconds left`;
+        }
+        return {
           id: obj.id,
-          occure_time: obj.occure_time,
+          occure_time: `${new Date(obj.occure_time * 1000).toString()} ${timeLeft ? '- ' + timeLeft : ''}`,
           place: obj.place,
           strength: obj.strength,
           coord_lat: obj.coord_lat,
-          coord_long: obj.coord_long,
-        }));
-        // {id: obj.id, occure_time: obj.occure_time, place: obj.place, strength: obj.strength,coord_lat: obj.coord_lat, coord_long: obj.coord_long }
-        this.setState({ posts });
+          coord_long: obj.coord_long
+        };
       });
+      this.setState({ posts });
+    });
   }
   handleTableChange = (pagination) => {
     this.setState({
@@ -86,8 +93,8 @@ class LogPage extends Component {
       {
         title: 'Strength',
         dataIndex: 'strength',
-        key: 'strength'
-
+        key: 'strength',
+        render: text => <Tag color="red">{text}</Tag>
       }
     ];
     return (
@@ -121,6 +128,7 @@ class LogPage extends Component {
                 textAlign: "center",
                 fontSize: "30px",
                 color: "black",
+                background: "#FFE3F2"
               }}
             >
               地震情報
