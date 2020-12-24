bf47ed92fb6ce3ec176eba4253ea94f98fef93b7
dphuong98 <32475252+dphuong98@users.noreply.github.com>
2020-12-25 04:35:57 +0700

    Signup function
    !no data escape, hash password

--- src/component/SignUp/SignUp.js
+++ src/component/SignUp/SignUp.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
 import { Modal, Form, Input } from "antd";
+import axios from "axios";
 
 class SignUp extends Component {
   state = {
+    user_count: 0,
+    valid: false,
     isSignUpModalVisible: false
   };
+
+  componentDidMount() {
+    axios
+        .get(`https://5fa8a7c7c9b4e90016e697f4.mockapi.io/api/jishin/user`)
+        .then((res) => {
+          const user_count = res.data.length;
+          this.setState({user_count});
+        })
+  }
 
   showModalSignUp = () => {
     this.setState({
@@ -13,14 +25,51 @@ class SignUp extends Component {
   };
 
   handleOkSignUp = () => {
-    this.setState({
-      isSignUpModalVisible: false
-    });
+    const user_detail = {
+      id: "",
+      name: this.state.name,
+      email: this.state.email,
+      telephone: this.state.telephone,
+      passsword: this.state.password,
+    };
+
+    user_detail.id = (parseInt(this.state.user_count)+1).toString();
+    axios.post(`https://5fa8a7c7c9b4e90016e697f4.mockapi.io/api/jishin/user`, user_detail)
   };
 
   handleCancelSignUp = () => {
     this.setState({
       isSignUpModalVisible: false
+    });
+  };
+
+  handleChangeName = (e) => {
+    this.setState({
+        name: e.target.value
+    });
+  };
+
+  handleChangeEmail = (e) => {
+    this.setState({
+        email: e.target.value
+    });
+  };
+
+  handleChangeTelephone = (e) => {
+    this.setState({
+        telephone: e.target.value
+    });
+  };
+
+  handleChangePassword = (e) => {
+    this.setState({
+        password: e.target.value
+    });
+  };
+
+  handleChangePassword2 = (e) => {
+    this.setState({
+        password2: e.target.value
     });
   };
 
@@ -45,35 +94,45 @@ class SignUp extends Component {
           <Form
             {...this.layout}
             name="basic"
-            initialValues={{ remember: true }}
+            initialValues={{ name: "", email: "", telephone: "", password: ""}}
             onOk={this.handleOkSignUp}
             onCancel={this.handleCancelSignUp}
           >
-            <Form.Item name="name" label="名前" rules={[{ required: true }]}>
-              <Input />
+            <Form.Item
+              label="名前"
+              name="name"
+              rules={[
+                {
+                  required: true,
+                  message: "名前を入力してください。" 
+                }
+              ]}>
+              <Input onChange={this.handleChangeName} />
             </Form.Item>
 
             <Form.Item
               label="メールアドレス"
-              name="username"
+              name="email"
               rules={[
                 {
                   required: true,
-                  message: "Please input your username!"
+                  message: "メールアドレスを入力してください。"
                 }
-              ]}
-            >
-              <Input />
+              ]}>
+              <Input onChange={this.handleChangeEmail}/>
             </Form.Item>
 
             <Form.Item
-              name={["user", "phonenumber"]}
               label="電話番号"
+              name="telephone"
               rules={[
-                { required: true, message: "Please input your phone number!" }
+                {
+                  required: true,
+                  message: "電話番号を入力してください。" 
+                }
               ]}
             >
-              <Input />
+              <Input onChange={this.handleChangeTelephone}/>
             </Form.Item>
 
             <Form.Item
@@ -82,24 +141,34 @@ class SignUp extends Component {
               rules={[
                 {
                   required: true,
-                  message: "Please input your password!"
+                  message: "パスワードを入力してください。"
                 }
               ]}
             >
-              <Input.Password />
+              <Input.Password onChange={this.handleChangePassword}/>
             </Form.Item>
 
             <Form.Item
               label="確認用のパスワード"
-              name="password"
+              name="password2"
               rules={[
                 {
                   required: true,
-                  message: "Please input your password again!"
-                }
+                  message: "確認用のパスワードを入力してください。"
+                },
+                ({ getFieldValue }) => ({
+                  validator(rule, value) {
+                    if (!value || getFieldValue('password') === value) {
+                      return Promise.resolve();
+                    }
+                    else {
+                      return Promise.reject("確認用のパスワードとパスワードが同じゃありません。");
+                    }
+                  },
+                }),
               ]}
             >
-              <Input.Password />
+              <Input.Password onChange={this.handleChangePassword2}/>
             </Form.Item>
           </Form>
         </Modal>
